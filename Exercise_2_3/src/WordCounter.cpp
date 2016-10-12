551d1a1ac45024f410a16526984ac11fa10aead2
matthias kr <matthias.kreileder@web.de>
2016-10-12 18:54:59 +0200

    NonAlpha Handling

--- Exercise_2_3/src/WordCounter.cpp
+++ Exercise_2_3/src/WordCounter.cpp
@@ -3,36 +3,53 @@
 #include <iostream>
 #include <exception>
 #include <cstdlib>
-WordCounter::WordCounter(std::string fileName) : m_invalidFileHandle(false){
-	
-	m_inputFile.open(fileName.c_str());
-
-	// check if we were able to open the file
-	if(!m_inputFile)
-		m_invalidFileHandle = true;
-
-
-	
-}
-
-int WordCounter::numWordsPriv(void){
-	int cnt = 0;
-	
-	while(!m_inputFile.eof()){
-		std::string tmp;
-		m_inputFile >> tmp;
-
-		// count only non-empty strings (this is an edge case in case the input file is empty
-		if(tmp.compare("") != 0)
-			cnt++;
-	}
-
-	return cnt;
+#include <ctype.h>
+#include <fstream>
+WordCounter::WordCounter(std::string fileName)
+	: m_fileName(fileName)
+{
 }
 
 int WordCounter::numWords(void){
-	if(m_invalidFileHandle)
+
+	std::ifstream inFile;
+	inFile.open(m_fileName.c_str());
+	// did 'open()' succeed?
+	if(!inFile)
 		return -1;
-	else
-		return numWordsPriv();
+
+	// cnt holds the number of words we encounter while
+	// we walk along the file
+	int cnt = 0;
+	while(!inFile.eof()){
+		std::string tmp;
+		inFile >> tmp;
+
+		// count only non-empty strings (this is an edge case in case the input file is empty
+		// and
+		// be aware on non-alphabetic characters
+		if(tmp.compare("") == 0 || !isWord(tmp))
+			continue;
+
+		++cnt;
+	}
+
+	inFile.close();
+	return cnt;
 }
+
+bool WordCounter::isWord(std::string s){
+	for(std::string::size_type i = 0; i < s.size(); i++){
+		if(isalpha(s[i])){
+			// encountered an alphabetic character => that is a word according to our definition
+			return true;
+		}
+	}
+
+	// made it here => not a word
+	return false;
+}
+
+
+
+
